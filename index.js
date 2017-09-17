e26f31de39130820f4e4272f38f28bb2fcf2851f
Parker Catalano <catalano.work@gmail.com>
2017-09-17 20:54:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 var name = "Joe"
 var height = "74"
 
+if (name === "Susan") {
+  console.log("The correct name is entered.")
+} else {
+  console.log("We are looking for the name of Susan!")
+}
 
 // Don't worry about this
 module.exports = { name, height
